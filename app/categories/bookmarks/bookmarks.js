bd0a7ca7484e0f4b4cd184b86e572851b3090176
KristenGarnier <garnier.kristen@icloud.com>
2016-05-26 14:41:40 +0200

    Building the architecture of the app

---
+++ app/categories/bookmarks/bookmarks.js
@@ -0,0 +1,6 @@
+import angular from 'angular'
+
+export default angular.module('categories.bookmarks', [
+  'eggly.models.categories',
+  'eggly.models.bookmarks'
+])
--- app/index.js
+++ app/index.js
@@ -3,11 +3,18 @@ import registerController from './controller'
 import normalize from '../assets/css/normalize.css'
 import eggly from '../assets/css/eggly.css'
 import animations from '../assets/css/animations.css'
+import categoriesModel from './models/categories-model'
+import bookmarksModel from './models/bookmark-model'
+import bookmarksModule from './categories/bookmarks/bookmarks'
+import categoriesModule from './categories/categories'
 
 if (ON_TEST) {
   require('angular-mocks/angular-mocks')
 }
 
-const ngModule = angular.module('Eggly', [])
+const ngModule = angular.module('Eggly', [
+  'categories',
+  'categories.bookmarks'
+])
 
 registerController(ngModule)
